2156037d74a4d1644d18c79c4b9c8ebe32b44a7e
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-08-03 10:43:23 -0700

    Create Board component

---
+++ src/components/Board/Board.js
@@ -0,0 +1,43 @@
+import "./board.scss";
+import data from "../../assets/data.json";
+import Square from "../Square/Square";
+const snakes = data.snakes;
+const ladders = data.ladders;
+
+function foundSnake(squareNummber) {
+  let result = undefined;
+  for (let i = 0; i < snakes.length; i++) {
+    if (snakes[i].source === squareNummber) {
+      result = snakes[i];
+    }
+  }
+  return result;
+}
+
+function foundLadder(squareNummber) {
+  let result = undefined;
+  for (let i = 0; i < ladders.length; i++) {
+    if (ladders[i].source === squareNummber) {
+      result = ladders[i];
+    }
+  }
+  return result;
+}
+
+const squaresArray = [];
+for (let i = 1; i <= 100; i++) {
+  squaresArray.push(
+    <Square
+      key={i}
+      squareNumber={i}
+      foundSnake={foundSnake(i)}
+      foundLadder={foundLadder(i)}
+    ></Square>
+  );
+}
+
+function App() {
+  return <div className='square-container'>{squaresArray}</div>;
+}
+
+export default App;
