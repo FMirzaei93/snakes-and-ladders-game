bb5a3fa41af076a6f0b63b5962e9dbab4d9d434c
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-08-03 10:42:54 -0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
+import Board from "./components/Board/Board";
 import "./App.css";
-import data from "./assets/data.json";
-import Square from "./components/Square";
-//import { createObjArray } from "./helper/utils";
-
-//const squareArray = createObjArray(data);
-
-const squareArray = data.squares.map((item) => (
-  <Square
-    key={item.squareNumber}
-    squareNumber={item.squareNumber}
-    snakeStatus={item.snakeStatus}
-    ladderStatus={item.ladderStatus}
-    dest={item.dest}
-  />
-));
 
 function App() {
-  return <div className='App'>{squareArray}</div>;
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Board />
+      </header>
+    </div>
+  );
 }
 
 export default App;
