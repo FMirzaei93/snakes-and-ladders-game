69f6bd36a07aef29407907997524e7032367e3a7
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-08-02 18:14:58 -0700

    receiving the initial data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import data from "./assets/data.json";
+import Square from "./components/Square";
+//import { createObjArray } from "./helper/utils";
+
+//const squareArray = createObjArray(data);
+
+const squareArray = data.squares.map((item) => (
+  <Square
+    key={item.squareNumber}
+    squareNumber={item.squareNumber}
+    snakeStatus={item.snakeStatus}
+    ladderStatus={item.ladderStatus}
+    dest={item.dest}
+  />
+));
 
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>{squareArray}</div>;
 }
 
 export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+const Square = ({ squareNumber, snakeStatus, ladderStatus, dest }) => {
+  return (
+    <div>
+      <h1>{squareNumber}</h1>
+    </div>
+  );
+};
+
+export default Square;
