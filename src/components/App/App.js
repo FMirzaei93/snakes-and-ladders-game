787c1000917be13b57b99cd4af23975b1218e9f8
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-08-03 16:55:46 -0700

    App component modifications

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Board from "../Board/Board";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className='container'>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
