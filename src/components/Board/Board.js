cd5b6ac18fa2a518b6caafb45b32aae8121c31f3
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-08-04 12:49:29 -0700

    adding bullet for each player

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,11 +3,14 @@ import data from "../../assets/data.json";
 import Square from "../Square/Square";
 import reducer, { initialStates } from "../../reducers";
 import React from "react";
+import redBullet from "../../assets/images/red-bullet.png";
+import blueBullet from "../../assets/images/blue-bullet.png";
+import classNames from "classnames";
 
 const snakes = data.snakes;
 const ladders = data.ladders;
 
-function foundSnake(squareNummber) {
+const foundSnake = (squareNummber) => {
   let result = undefined;
   for (let i = 0; i < snakes.length; i++) {
     if (snakes[i].source === squareNummber) {
@@ -15,9 +18,9 @@ function foundSnake(squareNummber) {
     }
   }
   return result;
-}
+};
 
-function foundLadder(squareNummber) {
+const foundLadder = (squareNummber) => {
   let result = undefined;
   for (let i = 0; i < ladders.length; i++) {
     if (ladders[i].source === squareNummber) {
@@ -25,46 +28,59 @@ function foundLadder(squareNummber) {
     }
   }
   return result;
-}
-
-const squaresArray = [];
-for (let i = 1; i <= 100; i++) {
-  squaresArray.push(
-    <Square
-      key={i}
-      squareNumber={i}
-      foundSnake={foundSnake(i)}
-      foundLadder={foundLadder(i)}
-    ></Square>
-  );
-}
+};
 
 const Board = () => {
   const [{ dice, winner, gameOver, turn, player1Pos, player2Pos }, dispatch] =
     React.useReducer(reducer, initialStates);
 
-  function rollClick() {
-    dispatch({ type: "roll" });
+  const squaresArray = [];
+  for (let i = 1; i <= 100; i++) {
+    squaresArray.push(
+      <Square
+        key={i}
+        squareNumber={i}
+        foundSnake={foundSnake(i)}
+        foundLadder={foundLadder(i)}
+        player1Pos={player1Pos}
+        player2Pos={player2Pos}
+      ></Square>
+    );
   }
 
-  React.useEffect(() => {
+  const updatePlayer = () => {
     dispatch({ type: "updatePlayer" });
-  }, [dice]);
+  };
+  const rollClick = (myCallback) => {
+    dispatch({ type: "roll" });
+    myCallback();
+  };
+
+  const bulletClassName = (turnNum) => {
+    return classNames({
+      bullet: true,
+      visible: turn === turnNum,
+      hidden: turn !== turnNum,
+    });
+  };
 
   return (
     <div className='main-container'>
       <div className='players-container'>
         <div className={turn === 1 ? "bold" : ""}>
+          <img className={bulletClassName(1)} src={redBullet} />
+
           <span>Player1: </span>
           <span>{player1Pos === 0 ? "has not entered" : player1Pos}</span>
         </div>
         <div className={turn === 2 ? "bold" : ""}>
+          <img className={bulletClassName(2)} src={blueBullet} />
           <span>Player2: </span>
           <span>{player2Pos === 0 ? "has not entered" : player2Pos}</span>
         </div>
       </div>
       <div className='square-container'>{squaresArray}</div>
-      <button className='roll' onClick={rollClick}>
+      <button className='roll' onClick={() => rollClick(updatePlayer)}>
         Roll
       </button>
       <span>Dice number: {dice}</span>
