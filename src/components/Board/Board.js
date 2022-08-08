00559cce52115bbcf8ba477a349b65810479175d
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-08-08 08:39:38 -0700

    removing some repetitive codes

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,31 +6,7 @@ import React from "react";
 import redBullet from "../../assets/images/red-bullet.png";
 import blueBullet from "../../assets/images/blue-bullet.png";
 import classNames from "classnames";
-
-const snakes = data.snakes;
-const ladders = data.ladders;
-
-const foundSnake = (playerPos) => {
-  let result = undefined;
-  for (let i = 0; i < snakes.length; i++) {
-    if (snakes[i].source === playerPos) {
-      result = snakes[i];
-      break;
-    }
-  }
-  return result;
-};
-
-const foundLadder = (playerPos) => {
-  let result = undefined;
-  for (let i = 0; i < ladders.length; i++) {
-    if (ladders[i].source === playerPos) {
-      result = ladders[i];
-      break;
-    }
-  }
-  return result;
-};
+import { foundSnake, foundLadder } from "../../helper/utils";
 
 const Board = () => {
   const [
@@ -38,48 +14,11 @@ const Board = () => {
     dispatch,
   ] = React.useReducer(reducer, initialStates);
 
-  // This function will create a random number for the die and then run the updatePlayer() function as a callback
-  async function rollClick() {
+  function rollClick() {
     dispatch({ type: "roll" });
   }
 
-  // React.useEffect(() => {
-  //   !checkDone && checkIsSnakeORLadder();
-  // }, [player1Pos, player2Pos]);
-
-  // React.useEffect(() => {
-  //   checkDone && dispatch({ type: "changeTurn" });
-  // }, [checkDone]);
-
-  // const checkIsSnakeORLadder = () => {
-  //   let currentPlayerPos;
-  //   if (turn === 1) currentPlayerPos = player1Pos;
-  //   else if (turn === 2) currentPlayerPos = player2Pos;
-
-  //   if (foundSnake(currentPlayerPos) !== undefined) {
-  //     console.log("snakeeee");
-  //     dispatch({
-  //       type: "applySnakeOrLadder",
-  //       payload: foundSnake(currentPlayerPos).dest,
-  //     });
-  //   } else if (foundLadder(currentPlayerPos) !== undefined) {
-  //     console.log("ladeeeer");
-  //     dispatch({
-  //       type: "applySnakeOrLadder",
-  //       payload: foundLadder(currentPlayerPos).dest,
-  //     });
-  //   } else {
-  //     dispatch({ type: "checkDone" });
-  //   }
-  // };
-
-  // React.useEffect(() => {
-  //   console.log("turrn in useeffect");
-  //   dispatch({ type: "changeTurn" });
-  // }, [player1Pos, player2Pos]);
-
   // This function will create the appropriate classname for the bullets next to each player's name.
-
   const bulletClassName = (turnNum) => {
     return classNames({
       bullet: true,
