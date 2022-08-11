d034fe2eca9c80b9f5377e32ddc168d8139f1c46
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-08-11 16:08:28 -0700

    explanation of each function

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -10,12 +10,13 @@ import data from "../../assets/data.json";
 import { createRandomNumber } from "../../helper/util";
 import Die from "../Die/Die";
 
-const foundSnake = (playerPos) => {
+// This function searches through the snakes list to check if it contains a specific square number, if so, it returns the information of that snake.
+const foundSnake = (squareNumber) => {
   const snakes = data.snakes;
 
   let result = undefined;
   for (let i = 0; i < snakes.length; i++) {
-    if (snakes[i].source === playerPos) {
+    if (snakes[i].source === squareNumber) {
       result = snakes[i];
       break;
     }
@@ -23,12 +24,13 @@ const foundSnake = (playerPos) => {
   return result;
 };
 
-const foundLadder = (playerPos) => {
+// This function searches through the ladders list to check if it contains a specific square number, if so, it returns the information of that ladder.
+const foundLadder = (squareNumber) => {
   const ladders = data.ladders;
 
   let result = undefined;
   for (let i = 0; i < ladders.length; i++) {
-    if (ladders[i].source === playerPos) {
+    if (ladders[i].source === squareNumber) {
       result = ladders[i];
       break;
     }
@@ -52,10 +54,13 @@ const Board = () => {
     dispatch,
   ] = React.useReducer(reducer, initialStates);
 
+  // Checking if any of the players reaches 100, to announce the game finished.
   if (!gameOver && (player1Pos === 100 || player2Pos === 100)) {
     dispatch({ type: "gameOver" });
   }
 
+  // This function checks if the current player's position is considered a snake/ladder, if so, the position will be updated with the defined destination of that snake/ladder,
+  //Then, the snake/ladder state gets updated.
   const applySnakeOrLadder = React.useCallback((currentPlayerPos) => {
     if (foundSnake(currentPlayerPos) !== undefined) {
       dispatch({ type: "snake", payload: true });
@@ -68,21 +73,32 @@ const Board = () => {
     return currentPlayerPos;
   }, []);
 
+  // This function simply reset the state of snake and ladder to false after each roll.
   const resetSnakeAndLadderStates = () => {
     if (snake) dispatch({ type: "snake", payload: false });
     if (ladder) dispatch({ type: "ladder", payload: false });
   };
 
+  // What this function does:
+  //1. It will generate a random number as the die number and updates the die state.
+  //2. Reset the state of snake and ladder.
+  //3. Check if the player is allowed to start the game(if it has thrown a 6?).
+  //4. If so, calls the 'applySnakeOrLadder' function to return the new player's position(check if it encounters a snake, ladder of neither)
+  //5. If the new player's position =<100 (the position must not exceed 100), it updates the player's state with the new returned value from 'applySnakeOrLadder'.
+  //6. If the player is not allowed, it checks if it just got a 6, and then update the player's permission.
+  //7. And eventually, it changes the turn.
   const rollClick = () => {
     let randomNum = createRandomNumber();
     dispatch({ type: "updateDie", payload: randomNum });
     resetSnakeAndLadderStates();
 
     let newPlayerPos = 0;
+    // Inner function to prevent repeating the codes for each player separately.
     const setPlayerNewPos = (playerStartPermission, playerPos) => {
       if (playerStartPermission) {
         newPlayerPos = applySnakeOrLadder(playerPos + randomNum);
-        dispatch({ type: "roll", payload: newPlayerPos });
+        if (newPlayerPos <= 100)
+          dispatch({ type: "roll", payload: newPlayerPos });
       } else {
         if (randomNum === 6) {
           dispatch({ type: "givePermission" });
@@ -99,11 +115,12 @@ const Board = () => {
     dispatch({ type: "changeTurn", payload: newPlayerPos });
   };
 
+  // This function initiates the game by resetting all the states.
   const playAgain = () => {
     dispatch({ type: "initiate" });
   };
 
-  // This function will create the appropriate classname for the bullets next to each player's name.
+  // This function will create the appropriate classnames for the bullets sitting next to each player's name.
   const bulletClassName = (turnNum) => {
     return classNames({
       bullet: true,
@@ -112,6 +129,7 @@ const Board = () => {
     });
   };
 
+  // This function will create the appropriate classnames for the player's information div.
   const playerInfoClassName = (turnNum) => {
     return classNames({
       playerInfo: true,
@@ -119,6 +137,7 @@ const Board = () => {
     });
   };
 
+  // An array of 100 squares that specifies if each square is a snake or ladder, and if any player is located there.
   const squaresArray = [];
   for (let i = 1; i <= 100; i++) {
     squaresArray.push(
