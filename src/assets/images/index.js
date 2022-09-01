9d9b919342924a39a65285c95d6c12d8d257fb52
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-09-01 14:02:47 -0700

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -12,9 +12,6 @@ import RedPiece from "./red-piece.png";
 import Snake from "./snake.png";
 import Star from "./star.png";
 
-//**************************** QUESTION *************************
-//Is it considered an object?
-
 const Images = {
   BlueBullet,
   BluePiece,
