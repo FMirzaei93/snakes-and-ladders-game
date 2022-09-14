3f57adcec4d9aa8ca40e03a6966c1b4c4645ace9
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-09-14 11:31:09 -0700

    Simplify the die's background-image

--- src/components/Die/Die.js
+++ src/components/Die/Die.js
@@ -1,67 +1,15 @@
-import images from "../../assets/images";
 import React from "react";
 import "./Die.scss";
 
-const { Die1, Die2, Die3, Die4, Die5, Die6 } = images;
-
-// I had a think of a way how we can simplify this code even more
-// You can use require to dynamically import the images based on the die number
-// const getDiceStyle = (dieNum) => {
-//   // Your one line would come here...
-
-//   return {
-//     backgroundImage: "url(" + img + ")",
-//     backgroundPosition: "center",
-//     backgroundSize: "cover",
-//     backgroundRepeat: "no-repeat",
-//   };
-// };
-
-// This will make your component to shrink from 46 lines to 4-5 lines
-
 const Die = ({ die }) => {
   const getDiceStyle = React.useMemo(() => {
-    const baseDieStyle = {
+    const img = require(`../../assets/images/die${die}.png`);
+    return {
+      backgroundImage: "url(" + img + ")",
       backgroundPosition: "center",
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
     };
-
-    switch (die) {
-      case 1:
-        return Object.assign(baseDieStyle, {
-          backgroundImage: "url(" + Die1 + ")",
-        });
-      case 2:
-        return Object.assign(baseDieStyle, {
-          backgroundImage: "url(" + Die2 + ")",
-        });
-      case 3:
-        return Object.assign(baseDieStyle, {
-          backgroundImage: "url(" + Die3 + ")",
-        });
-      case 4:
-        return Object.assign(baseDieStyle, {
-          backgroundImage: "url(" + Die4 + ")",
-        });
-      case 5:
-        return Object.assign(baseDieStyle, {
-          backgroundImage: "url(" + Die5 + ")",
-        });
-      case 6:
-        return Object.assign(baseDieStyle, {
-          backgroundImage: "url(" + Die6 + ")",
-        });
-      default:
-        return Object.assign(baseDieStyle, {
-          backgroundImage: "url(" + Die1 + ")",
-        });
-      // OR
-      //return {
-      //   ...baseDieStyle,
-      //   backgroundImage: "url(" + die1 + ")",
-      // }
-    }
   }, [die]);
 
   return <div className='die-face' style={getDiceStyle}></div>;
